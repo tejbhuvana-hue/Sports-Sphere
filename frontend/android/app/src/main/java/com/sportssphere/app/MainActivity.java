2673f5041a612430698a24e266737555cfa00714
TEJA AS <tejbhuvana220@gmail.com>
2026-09-02 20:13:28 +0530

    app

---
+++ frontend/android/app/src/main/java/com/sportssphere/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.sportssphere.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sportssphere.app',
+  appName: 'SportsSphere',
+  webDir: 'dist'
+};
+
+export default config;
